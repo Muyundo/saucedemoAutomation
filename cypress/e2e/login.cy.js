071549741d7eec3eec42576301811316700eb4ea
MUYUNDO BRIAN <muyundobrian@gmail.com>
2025-07-17 14:58:50 +0300

    updated login test with invalid credentials

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,5 +10,15 @@ it('access and log in with valid credentials', () => {
 
 })
 
+it('access and log in with invalid credentials', () => {
+    cy.visit('https://www.saucedemo.com/')
+    cy.url().should('include', 'saucedemo.com')
+    cy.get('#user-name').type('invalid_user')
+    cy.get('#password').type('invalid_password')
+    cy.get('#login-button').click()
+    cy.get('.error-message-container').should('be.visible')
+    cy.get('.error-message-container').should('contain', 'Epic sadface: Username and password do not match any user in this service')
+})
+
 
 })
